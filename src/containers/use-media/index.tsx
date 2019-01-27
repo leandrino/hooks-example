a3cc321b369eefa432cf91d3cfff4830a4550ffe
Leandro Alexandrino <leandro.alexandrino@softplan.com.br>
2019-01-27 20:02:56 -0200

    refactoring use media container

---
+++ src/containers/use-media/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { IDataImages } from '../../interfaces/images';
+import data from '../../data/images';
+import useMedia from '../../hooks/use-media';
+import {
+  ImageWrapper,
+  ImageGroup,
+  ImageContainer
+} from '../../commons/image-grid';
+
+const UseMedia = () => {
+  const columnCount = useMedia(
+    ['(min-width: 1500px)', '(min-width: 1000px)', '(min-width: 600px)'],
+    [5, 4, 3],
+    2
+  );
+
+  let columnHeights: any[] = new Array(columnCount).fill(0);
+  let columns: any[] = new Array(columnCount).fill(0).map(() => []);
+
+  data.forEach((item: IDataImages) => {
+    const shortColumnIndex = columnHeights.indexOf(Math.min(...columnHeights));
+    columns[shortColumnIndex].push(item);
+    columnHeights[shortColumnIndex] += item.height;
+  });
+
+  return (
+    <ImageWrapper>
+      {columns.map((column: any[], index: number) => (
+        <ImageGroup key={`image-group-${index}`}>
+          {column.map((item: IDataImages, index: number) => (
+            <ImageContainer
+              key={`image-container-${index}`}
+              height={item.height}
+              width={item.width}>
+              <img src={item.image} alt="" />
+            </ImageContainer>
+          ))}
+        </ImageGroup>
+      ))}
+    </ImageWrapper>
+  );
+};
+
+export default UseMedia;
